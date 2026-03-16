4a6fd8dd7aba09512808b793d0ef9177d32e358c
20226026-dopg <phangiado1107@gmail.com>
2026-03-16 17:43:41 +0700

    feat: integrate Clerk for authentication and update project dependencies

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-import { Image } from "expo-image";
-export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-lg font-bold text-green-500">Hello World!</Text>
-    </View>
-  );
-}
